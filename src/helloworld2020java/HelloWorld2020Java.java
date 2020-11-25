4c67352999c6f806df7a3101747da1297b3779c4
MrZebarth <kris.zebarth@ocsb.ca>
2020-11-25 11:41:22 -0500

    Initial commit

---
+++ src/helloworld2020java/HelloWorld2020Java.java
@@ -0,0 +1,21 @@
+
+package helloworld2020java;
+
+/**
+ *
+ * @author 
+ * Class: ICS3U
+ * Teacher: Mr. Zebarth
+ * Description: Print out "Hello world!"
+ */
+public class HelloWorld2020Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Your code goes HERE
+        
+    }
+    
+}
